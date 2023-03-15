4c9429f147487b66031e91efac45f6a74612cbe4
berrujaime <berrujaime@gmail.com>
2023-03-15 15:39:40 +0100

    vercel config

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/users", require("./app_server/routes/users"));
 
