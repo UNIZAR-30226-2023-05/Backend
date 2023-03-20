c1b7f7d51a9a07b91bedea69ec6de2c4dbdc8fe1
iKerKa <iker48140@gmail.com>
2023-03-20 19:35:12 +0100

    inicio pruebas de software

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ const port = process.env.PORT || 3000;
 
 app.use("/users", require("./app_server/routes/users"));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
