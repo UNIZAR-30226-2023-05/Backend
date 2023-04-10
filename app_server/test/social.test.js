42e16e569378c880945cf3defc50ed8b4f9b8d4c
berrujaime <berrujaime@gmail.com>
2023-04-10 16:35:28 +0200

    Se pueden ejecutar ambos test a la vez usando npm test

---
+++ app_server/test/social.test.js
@@ -0,0 +1,9 @@
+const { testSolicitud, testGetSolicitudes, testGetAmigos, testRechazarSolicitudes } = require("./ttsFunctions/socialTest");
+
+describe('Tests parte 2', () => {
+    //Tests de /social
+    testSolicitud();
+    testGetSolicitudes();
+    testGetAmigos();
+    testRechazarSolicitudes();
+});
--- app_server/test/user.test.js
+++ app_server/test/user.test.js
@@ -1,12 +1,10 @@
 //Importar los ficheros de test y ejecutarlos
 //
 const { testRegistro, testLogin, delTestUser, testUpdateUser, testGetUserInfo, testGetUserId } = require("./ttsFunctions/userTest");
-const { testSolicitud, testGetSolicitudes, testGetAmigos, testRechazarSolicitudes } = require("./ttsFunctions/socialTest");
 
 //Ejecutar los tests al mismo tiempo
-describe('Tests', () => {
+describe('Tests parte 1', () => {
     //Hay que ejecutar los test por separados porque si no da conflictos la BD
-    /*
     //Tests de /users
     delTestUser();
     testRegistro();
@@ -14,11 +12,4 @@ describe('Tests', () => {
     testUpdateUser();
     testGetUserInfo();
     testGetUserId();
-    */
-
-    //Tests de /social
-    testSolicitud();
-    testGetSolicitudes();
-    testGetAmigos();
-    testRechazarSolicitudes();
 });
