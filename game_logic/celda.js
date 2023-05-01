238b94616192124883d1222b98bb9223d26863e8
berrujaime <berrujaime@gmail.com>
2023-05-01 13:17:55 +0200

    Actualización celda para soportar estadísticas

--- game_logic/celda.js
+++ game_logic/celda.js
@@ -1,21 +1,23 @@
 // Clase celda
 class Celda {
-    constructor(id, nueva, turno, penalizacion) {
+    constructor(id, nueva, turno, penalizacion, caidoOca, caidoCalavera) {
         this.id = id; //Número de la celda en el tablero
         this.nueva = nueva; //Número de la nueva casilla en la que se encuentra
         this.turno = turno; //Booleano que indica si se vuelve a tener el turno o no
         this.penalizacion = penalizacion; //Número de turnos perdidos
+        this.caidoOca = caidoOca; //Booleano que indica si se ha caído en una oca
+        this.caidoCalavera = caidoCalavera; //Booleano que indica si se ha caído en una calavera
     }
     
     // Método execute. Será implementado por las clases hijas.
     execute() {
-        return { nueva: this.nueva, turno: this.turno, penalizacion: this.penalizacion };
+        return { nueva: this.nueva, turno: this.turno, penalizacion: this.penalizacion, caidoOca: this.caidoOca, caidoCalavera: this.caidoCalavera };
     }
 }
 
 class CeldaNormal extends Celda {
     constructor(id) {
-        super(id, id, false, 0); //No avanza ni retrocede,no se vuelve a tener el turno y no se pierde ningún turno adicional
+        super(id, id, false, 0, false, false); //No avanza ni retrocede,no se vuelve a tener el turno y no se pierde ningún turno adicional
     }
 }
 
@@ -23,43 +25,43 @@ class CeldaOca extends Celda{
     constructor(id) {
         switch (id) {
             case 5:
-                super(id, 9, true, 0);
+                super(id, 9, true, 0, true, false);
                 break;
             case 9:
-                super(id, 14, true, 0);
+                super(id, 14, true, 0, true, false);
                 break;
             case 14:
-                super(id, 18, true, 0);
+                super(id, 18, true, 0, true, false);
                 break;
             case 18:
-                super(id, 23, true, 0);
+                super(id, 23, true, 0, true, false);
                 break;
             case 23:
-                super(id, 27, true, 0);
+                super(id, 27, true, 0, true, false);
                 break;
             case 27:
-                super(id, 32, true, 0);
+                super(id, 32, true, 0, true, false);
                 break;
             case 32:
-                super(id, 36, true, 0);
+                super(id, 36, true, 0, true, false);
                 break;
             case 36:
-                super(id, 41, true, 0);
+                super(id, 41, true, 0, true, false);
                 break;
             case 41:
-                super(id, 45, true, 0);
+                super(id, 45, true, 0, true, false);
                 break;
             case 45:
-                super(id, 50, true, 0);
+                super(id, 50, true, 0, true, false);
                 break;
             case 50:
-                super(id, 54, true, 0);
+                super(id, 54, true, 0, true, false);
                 break;
             case 54:
-                super(id, 59, true, 0);
+                super(id, 59, true, 0, true, false);
                 break;
             case 59:
-                super(id, 63, true, 0);
+                super(id, 63, true, 0, true, false);
                 break;
             default:
                 throw new Error("Error: el id de la celda no es válido");
@@ -72,10 +74,10 @@ class CeldaPuente extends Celda {
     constructor(id) {
         switch(id){
             case 6:
-                super(id, 12, true, 0);
+                super(id, 12, true, 0, false, false);
                 break;
             case 12:
-                super(id, 6, true, 0);
+                super(id, 6, true, 0, false, false);
                 break;
             default:
                 throw new Error("Error: el id de la celda no es válido");
@@ -86,35 +88,35 @@ class CeldaPuente extends Celda {
 
 class CeldaPosada extends Celda {
     constructor(id) {
-        super(id, id, false, 1);
+        super(id, id, false, 1, false, false);
     }
 }
 
 class CeldaPozo extends Celda {
     constructor(id) {
-        super(id, id, false, 2);
+        super(id, id, false, 2, false, false);
     }
 }
 
 class CeldaLaberinto extends Celda {
     constructor(id) {
-        super(id, id, false, 3);
+        super(id, id, false, 3, false, false);
     }
 }
 
 class CeldaCarcel extends Celda {
     constructor(id) {
-        super(id, id, false, 4);
+        super(id, id, false, 4, false, false);
     }
 }
 
 class CeldaDados extends Celda {
     constructor(id) {
         if(id == 53) {
-            super(id, 26, true, 0);
+            super(id, 26, true, 0, false, false);
         }
         else if(id == 26) {
-            super(id, 53, true, 0);
+            super(id, 53, true, 0, false, false);
         }
         else {
             throw new Error("Error: el id de la celda no es válido");
@@ -124,7 +126,7 @@ class CeldaDados extends Celda {
 
 class CeldaCalavera extends Celda {
     constructor(id) {
-        super(id, 1, false, 0);
+        super(id, 1, false, 0, false, true);
     }
 }
 
